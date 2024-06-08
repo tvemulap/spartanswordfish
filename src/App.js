2490bcca166992d885cf26af5e7f153344bc1753
Teja <tvemulap@icloud.com>
2024-06-07 21:41:20 -0700

    Dashboard Fix
    dashboard was defaulting to page 6/6, fixed to page 1/6

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <p><iframe title="Parallel Planetfall" width="1280" height="765" src="https://app.powerbi.com/view?r=eyJrIjoiMjcwYTJiNjItYTBiMS00NjczLThhNjgtYjEyZWRjMjdkMzcxIiwidCI6IjRhNDI4NzQ0LWQ1MDgtNDI1YS1iM2Q1LTQ0Y2Q1YjkwYjkzOSIsImMiOjZ9" frameborder="0" allowFullScreen="true"></iframe></p>
+      <p><iframe title="Parallel Planetfall" width="1280" height="765" src="https://app.powerbi.com/view?r=eyJrIjoiMjcwYTJiNjItYTBiMS00NjczLThhNjgtYjEyZWRjMjdkMzcxIiwidCI6IjRhNDI4NzQ0LWQ1MDgtNDI1YS1iM2Q1LTQ0Y2Q1YjkwYjkzOSIsImMiOjZ9&pageName=254b8012b2e7084b7e60" frameborder="0" allowFullScreen="true"></iframe></p>
       </header>
     </div>
   );
