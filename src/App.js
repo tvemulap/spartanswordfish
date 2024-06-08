29692b4f0d2d7a9b013ef4213e40cfb66e34d22b
Teja <tvemulap@icloud.com>
2024-06-07 20:08:38 -0700

    Wrong setup, updating to React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p><iframe title="Parallel Planetfall" width="1024" height="612" src="https://app.powerbi.com/view?r=eyJrIjoiMjcwYTJiNjItYTBiMS00NjczLThhNjgtYjEyZWRjMjdkMzcxIiwidCI6IjRhNDI4NzQ0LWQ1MDgtNDI1YS1iM2Q1LTQ0Y2Q1YjkwYjkzOSIsImMiOjZ9&pageName=254b8012b2e7084b7e60" frameborder="0" allowFullScreen="true"></iframe></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
