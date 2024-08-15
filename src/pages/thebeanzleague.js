053d7f4bcd927cc0700eabe6e9b504aa3d61080a
Teja <tvemulap@icloud.com>
2024-08-15 09:17:22 -0700

    More Minor Updates

---
+++ src/pages/thebeanzleague.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ListGroup from 'react-bootstrap/ListGroup';
+import './AboutMe.css';
+
+function BeanzLeague() {
+  return (
+    <div  className="beanz-header">
+            <img src="StrawHatTriarchy.png" alt="Straw Hat Triarchy" height="286" class="rounded"/>
+    </div>
+  );
+}
+
+export default BeanzLeague;
