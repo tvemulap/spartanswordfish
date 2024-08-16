f616170c1e216c8fd833b62b400128ee120f4aab
Teja <tvemulap@icloud.com>
2024-08-15 20:59:11 -0700

    clean up 8/15

--- src/NavBar.js
+++ src/NavBar.js
@@ -27,7 +27,7 @@ function NavBar() {
           <Nav.Link href="#" disabled>Resume</Nav.Link>
           <NavDropdown title="Projects" id="basic-nav-dropdown">
               <NavDropdown.Item href="/parallel">Parallel Match Stats</NavDropdown.Item>
-              <NavDropdown.Item href="/beanzleague">The Beanz League</NavDropdown.Item>
+              <NavDropdown.Item href="/beanzleague">Straw Hat Triarchy</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item href="#" disabled>Movie Reviews</NavDropdown.Item>
               <NavDropdown.Item href="#" disabled>Some web3 thing</NavDropdown.Item>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,56 +7,56 @@ import './Home.css';
 function Home() {
   return (
     <div  className="home-header">
-      <div class="container">
-        <div class="row mt-4">
-          <div class="col-sm">
-            <img src="Azuki6518.png" class="rounded float-left" alt="Azuki #6518" height="400"></img>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-sm">
+            <img src="Azuki6518.png" className="rounded float-left" alt="Azuki #6518" height="400"></img>
           </div>
-          <div class="col-sm">
-            <img src="AzukiElemental6583.png" class="rounded float-left" alt="Elemental #6583" height="400"></img>
+          <div className="col-sm">
+            <img src="AzukiElemental6583.png" className="rounded float-left" alt="Elemental #6583" height="400"></img>
           </div>
-          <div class="col-sm">
-            <img src="AzukiElemental12983.png" class="rounded float-left" alt="Elemental #12983" height="400"></img>
-          </div>
-        </div>
-      </div>
-
-      <div class="container">
-        <div class="row mt-4">
-          <div class="col-sm">
-            <img src="Bean2088.png" class="rounded float-left" alt="Beanz #2088" height="400"></img>
-          </div>
-          <div class="col-sm">
-            <img src="AzukiElemental12734.png" class="rounded float-left" alt="Elemental #12734" height="400"></img>
-          </div>
-          <div class="col-sm">
-            <img src="Bean9914.png" class="rounded float-left" alt="Beanz #9914" height="400"></img>
+          <div className="col-sm">
+            <img src="AzukiElemental12983.png" className="rounded float-left" alt="Elemental #12983" height="400"></img>
           </div>
         </div>
       </div>
 
-      <div class="container">
-        <div class="row mt-4">
-          <div class="col-sm">
-            <img src="Odeda.avif" class="rounded float-left" alt="Odeda" height="400"></img>
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-sm">
+            <img src="Bean2088.png" className="rounded float-left" alt="Beanz #2088" height="400"></img>
           </div>
-          <div class="col-sm">
-            <img src="Yustus.avif" class="rounded float-left" alt="Yustus" height="400"></img>
+          <div className="col-sm">
+            <img src="AzukiElemental12734.png" className="rounded float-left" alt="Elemental #12734" height="400"></img>
           </div>
-          <div class="col-sm">
-            <img src="Vladi.avif" class="rounded float-left" alt="Vladi" height="400"></img>
+          <div className="col-sm">
+            <img src="Bean9914.png" className="rounded float-left" alt="Beanz #9914" height="400"></img>
           </div>
         </div>
       </div>
 
-      <div class="container">
-        <div class="row mt-4 mb-4">
-          <div class="col-sm">
+      <div className="container">
+        <div className="row mt-4">
+          <div className="col-sm">
+            <img src="Odeda.avif" className="rounded float-left" alt="Odeda" height="400"></img>
           </div>
-          <div class="col-sm">
-            <img src="Keeper5538.avif" class="rounded float-center" alt="Keeper #5538" height="400"></img>
+          <div className="col-sm">
+            <img src="Yustus.avif" className="rounded float-left" alt="Yustus" height="400"></img>
           </div>
-          <div class="col-sm">
+          <div className="col-sm">
+            <img src="Vladi.avif" className="rounded float-left" alt="Vladi" height="400"></img>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row mt-4 mb-4">
+          <div className="col-sm">
+          </div>
+          <div className="col-sm">
+            <img src="Keeper5538.avif" className="rounded float-center" alt="Keeper #5538" height="400"></img>
+          </div>
+          <div className="col-sm">
           </div>
         </div>
       </div>
