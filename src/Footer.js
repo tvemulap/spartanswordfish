af0334f5983113d7a3e704ac89632d4461485b25
Teja <tvemulap@icloud.com>
2024-08-16 10:59:10 -0700

    Minor updates #4

--- src/Footer.js
+++ src/Footer.js
@@ -2,13 +2,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Footer() {
     return (
-        <footer class="py-3 my-4">
-            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
-                <li class="nav-item"><a href="/aboutme" class="nav-link px-2 text-muted">About Me</a></li>
-                <li class="nav-item"><a href="/" disabled class="nav-link px-2 text-muted">Resume</a></li>
+        <footer className="py-3 my-4">
+            <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                <li className="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+                <li className="nav-item"><a href="/aboutme" class="nav-link px-2 text-muted">About Me</a></li>
             </ul>
-            <p class="text-center text-muted">spartanswordfish</p>
+            <p className="text-center text-muted">spartanswordfish</p>
         </footer>
     );
 }
--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -13,9 +13,9 @@ function AboutMe() {
                 <Card.Title>teja.eth</Card.Title>
                 <Card.Subtitle className="mb-2 text-muted">tvemulap</Card.Subtitle>
                 <ListGroup className="list-group-flush">
+                <ListGroup.Item> Diehard Steelers Fan</ListGroup.Item>
+                <ListGroup.Item>Staunch Earthen Ally</ListGroup.Item>
                 <ListGroup.Item>Resident of The Garden</ListGroup.Item>
-                <ListGroup.Item>Staunch Earthen Supporter</ListGroup.Item>
-                <ListGroup.Item>Steelers Fan</ListGroup.Item>
             </ListGroup>
             <Button className="text-center" variant="outline-secondary" href="https://x.com/tvemulap_eth"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
